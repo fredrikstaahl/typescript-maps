5fb8afbf0fe68e8414bfa70877994aef9f7d1c65
fredrikstahl <fredrik@fredrikniklas.com>
2022-08-11 08:21:29 +0200

    finish map project

---
+++ src/CustomMap.ts
@@ -0,0 +1,43 @@
+import { Company } from './Company';
+import { User } from './User';
+
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(elementId: string) {
+    new google.maps.Map(document.getElementById(elementId)!, {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    console.log('Adding marker with location: ' + JSON.stringify(mappable.location));
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
